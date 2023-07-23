464458a98b898c71036d6c18947d14e1a92ba2f3
tanqin <cn.tanqin@qq.com>
2023-07-23 14:48:16 +0800

    feat: 生成规格参数动态域

--- shop-parent/shop-service/shop-service-search/src/main/java/com/shop/search/service/impl/SkuServiceImpl.java
+++ shop-parent/shop-service/shop-service-search/src/main/java/com/shop/search/service/impl/SkuServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class SkuServiceImpl implements SkuService {
@@ -26,6 +27,14 @@ public class SkuServiceImpl implements SkuService {
         Result<List<Sku>> skuListResult = skuFeign.findAll();
         // 将 Sku 数据转为 SkuInfo 数据
         List<SkuInfo> skuInfoList = JSON.parseArray(JSON.toJSONString(skuListResult.getData()), SkuInfo.class);
+        // 生成规格参数动态域
+        for (SkuInfo skuInfo : skuInfoList) {
+            // 将 spec 字符串转为 map 形式
+            Map<String, Object> specMap = JSON.parseObject(skuInfo.getSpec());
+            // 设置 specMap
+            skuInfo.setSpecMap(specMap);
+        }
+
         // 将 SkuInfo 数据存入 ES 中
         skuEsMapper.saveAll(skuInfoList);
 
